40494faa96f67a0cb3a3194a3a981f99098f6a16
BlakeKessler <blakekessler123@gmail.com>
2025-04-15 14:38:38 -0400

    reduce density of random product terms

--- app/src/ProdTerm.cpp
+++ app/src/ProdTerm.cpp
@@ -7,7 +7,7 @@
 #include "pair.hpp"
 
 inform::ProdTerm inform::ProdTerm::makeRand() {
-   mcsl::pair<uint32, uint32> data = std::bit_cast<mcsl::pair<uint32,uint32>>(mcsl::rand());
+   mcsl::pair<uint32, uint32> data = std::bit_cast<mcsl::pair<uint32,uint32>>(mcsl::rand() & mcsl::rand());
    data.first &= data.second; //ensure canonical representation (all masked-out terms are 0)
    return ProdTerm{data.first, data.second};
 }
