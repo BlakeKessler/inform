a51f9359c416014369776520bd2fc398b3e02739
BlakeKessler <blakekessler123@gmail.com>
2025-04-15 20:45:23 -0400

    ProdTerm::operator[]

--- app/header/ProdTerm.hpp
+++ app/header/ProdTerm.hpp
@@ -5,12 +5,21 @@
 
 //!product term
 struct inform::ProdTerm {
+   public:
+      enum Status : ubyte {
+         FALSE = false,
+         TRUE = true,
+         FLOATING
+      };
+
    private:
       uint32 _varVals; //value of variables
       uint32 _varsMask; //which variables are actually part of the term
    public:
       ProdTerm(uint32 vars = 0, uint32 mask = 0):_varVals{vars},_varsMask{mask} {}
       static ProdTerm makeRand();
+
+      Status operator[](ubyte i);
 
 } alignas(uint64);
 
--- app/src/ProdTerm.cpp
+++ app/src/ProdTerm.cpp
@@ -5,11 +5,20 @@
 
 #include "rand.hpp"
 #include "pair.hpp"
+#include "assert.hpp"
 
 inform::ProdTerm inform::ProdTerm::makeRand() {
    mcsl::pair<uint32, uint32> data = std::bit_cast<mcsl::pair<uint32,uint32>>(mcsl::rand() & mcsl::rand());
    data.first &= data.second; //ensure canonical representation (all masked-out terms are 0)
    return ProdTerm{data.first, data.second};
+}
+
+inform::ProdTerm::Status inform::ProdTerm::operator[](ubyte i) {
+   assume(i < (8 * sizeof(_varsMask)));
+   if (!((_varsMask >> i) & 1)) {
+      return Status::FLOATING;
+   }
+   return (_varVals >> i) & 1 ? TRUE : FALSE;
 }
 
 #endif
